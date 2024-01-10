0218a9ccc58fd5f01e5384b3ee3180f1d57c3e17
Pushkar Verma <pushkar7397@gmail.com>
2024-01-11 00:43:26 +0530

    Function declaration and return type

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades" // := is only used when declaring a new variable
-	card = "Five of Diamonds" // = is used to reassign a variable
+
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string { // function returns a string
+	return "Five of Diamonds"
 }
