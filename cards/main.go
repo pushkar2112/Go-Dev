c315725b94fb67675f1a10690abffcaea3310b48
Pushkar Verma <pushkar7397@gmail.com>
2024-01-11 00:37:45 +0530

    Variable declaration and assignment

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades" // := is only used when declaring a new variable
+	card = "Five of Diamonds" // = is used to reassign a variable
+
+	fmt.Println(card)
+}
